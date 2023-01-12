423d41b8c80377fafb9f134b907e07d3e700b0cd
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-01-12 20:59:25 +0100

    AddCounter other counter on my page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCalculator,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Counter from "./components/Counter";
+import AddCounter from "./components/AddCounter";
+
+library.add(faCalculator, faMinus, faPlus);
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  /*  Je créer un state AddCounter pour ajouter les compteurs supplémentaires :)  */
+  const [addCounter, setAddCounter] = useState(1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AddCounter
+        state={AddCounter}
+        setState={setAddCounter}
+        className="addCounter"
+      >
+        Add Counter
+      </AddCounter>
+
+      <main>
+        <div className="counter">
+          <div>
+            <div
+              className="minus"
+              onClick={() => {
+                setCounter(counter - 1);
+              }}
+            >
+              {counter > 0 && (
+                <FontAwesomeIcon icon="fa-solid fa-minus" size="4x" />
+              )}
+            </div>
+            <p>{counter}</p>
+
+            <div
+              className="plus"
+              onClick={() => {
+                setCounter(counter + 1);
+              }}
+            >
+              {counter < 10 && (
+                <FontAwesomeIcon icon="fa-solid fa-plus" size="4x" />
+              )}
+            </div>
+          </div>
+          <button
+            className="reset"
+            onClick={() => {
+              setCounter(0);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
